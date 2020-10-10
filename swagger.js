ec3f329dccf4120ee8e6b4fae4b2ab88765041b3
MukundKhunt <khuntmukund123@gmail.com>
2020-10-10 19:26:20 +0530

    Updated

--- swagger.js
+++
@@ -1,5 +0,0 @@
-const swaggerUI = require('swagger-ui-express'),
-    express = require('express'),
-    app = express()
-app.use('/api/swag', swaggerUI.serve, swaggerUI.setup(require('./swagger.json')))
-app.listen(port = process.env.PORT || 5000, () => console.log(`Server running on port : ${port}`))
