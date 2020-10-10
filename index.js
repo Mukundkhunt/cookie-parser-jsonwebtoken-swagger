e24bd2276d10c7e2888fcf0b48ab2c7a71b0a0df
MukundKhunt <khuntmukund123@gmail.com>
2020-10-10 11:44:26 +0530

    v1.0.1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express'),
-    cookierParser = require('cookie-parser')
+    cookierParser = require('cookie-parser'),
+    morgan = require('morgan')
 
 
 const app = express()
 app.use(cookierParser())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     console.log("Cookies", req.cookies)
@@ -11,4 +13,4 @@ app.get('/', (req, res) => {
     res.send(200)
 })
 
-app.listen(port = process.env.PORT || 5000, () => console.log(`Serever running on port : ${port}`))
+app.listen(port = process.env.PORT || 5000, () => console.log(`Server running on port : ${port}`))
